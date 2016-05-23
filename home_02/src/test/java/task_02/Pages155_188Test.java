e16ec861b3ca62a059a5df840194dcf9b5674549
dshvedchenko <dshvedchenko@gmail.com>
2016-05-23 06:58:23 +0300

    add queries upto 182

--- home_02/src/test/java/task_02/Pages155_188Test.java
+++ home_02/src/test/java/task_02/Pages155_188Test.java
@@ -63,7 +63,7 @@ public class Pages155_188Test {
                 "WHERE REP_OFFICE = OFFICE;";
 
         ResultSet rs = stmt.executeQuery(sqlQuery);
-        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sam Clark","New York",  "Eastern"});
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sam Clark", "New York", "Eastern"});
     }
 
     @Test
@@ -85,7 +85,7 @@ public class Pages155_188Test {
                 "REP_OFFICE = OFFICE;";
 
         ResultSet rs = stmt.executeQuery(sqlQuery);
-        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sam Clark","New York",  "Eastern"});
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sam Clark", "New York", "Eastern"});
     }
 
     @Test
@@ -134,5 +134,274 @@ public class Pages155_188Test {
 
     }
 
+    @Test
+    public void queryParentChildJoinsMultiKeyAlter_p163() throws SQLException {
 
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT , DESCRIPTION\n" +
+                "FROM ORDERS JOIN PRODUCTS\n" +
+                "ON MFR = MFR_ID\n" +
+                "AND PRODUCT = PRODUCT_ID";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"112992", "760.00", "Size 2 Widget"});
+
+    }
+
+    @Test
+    public void queryParentChildNaturalJoin_p164() throws SQLException {
+
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT , DESCRIPTION FROM ORDERS NATURAL JOIN PRODUCTS ;";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"112961", "31500.00", "Size 1 Wiget"});
+    }
+
+    @Test
+    public void queryParentChildJoinMulti_p164() throws SQLException {
+
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT , DESCRIPTION FROM ORDERS JOIN PRODUCTS " +
+                "ON ORDERS.MFR = PRODUCTS.MFR_ID " +
+                "AND ORDERS.PRODUCT = PRODUCTS.PRODUCT_ID;";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"112992", "760.00", "Size 2 Widget"});
+    }
+
+    @Test
+    public void queryParentChildJoin3Table_p165() throws SQLException {
+
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT, COMPANY, NAME\n" +
+                "FROM ORDERS , CUSTOMERS , SALESREPS\n" +
+                "WHERE CUST = CUST_NUM\n" +
+                "AND REP = EMPL_NUM\n" +
+                "AND AMOUNT > 25000.00;";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"112987", "27500.00", "Acme Mfg.", "Bill Adams"});
+    }
+
+    @Test
+    public void queryParentChildJoin3TableAlt_p165() throws SQLException {
+
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT, COMPANY, NAME\n" +
+                "FROM ORDERS JOIN CUSTOMERS ON CUST = CUST_NUM \n " +
+                "JOIN SALESREPS ON REP = EMPL_NUM\n" +
+                "WHERE AMOUNT > 25000.00;";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"112987", "27500.00", "Acme Mfg.", "Bill Adams"});
+    }
+
+    @Test
+    public void queryParentChildJoin3TableCustRep_p166() throws SQLException {
+
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT, COMPANY, NAME\n" +
+                "FROM ORDERS JOIN CUSTOMERS ON CUST = CUST_NUM \n " +
+                "JOIN SALESREPS ON CUST_REP = EMPL_NUM\n" +
+                "WHERE AMOUNT > 25000.00;";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"113069", "31350.00", "Chen Associates", "Paul Cruz"});
+    }
+
+    @Test
+    public void queryParentChildJoin3TableCustRepOffice_p167() throws SQLException {
+
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT, COMPANY, NAME, CITY\n" +
+                "FROM ORDERS , CUSTOMERS , SALESREPS, OFFICES\n" +
+                "WHERE CUST = CUST_NUM\n" +
+                "AND CUST_REP = EMPL_NUM\n" +
+                "AND REP_OFFICE = OFFICE\n" +
+                "AND AMOUNT > 25000.00;\n";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"112961", "31350.00", "J.P. Sinclair", "Sam Clark", "New York"});
+    }
+
+    @Test
+    public void queryParentChildJoinOrderHire_p169() throws SQLException {
+
+        String sqlQuery = "SELECT ORDER_NUM, AMOUNT , ORDER_DATE , NAME\n" +
+                "FROM ORDERS , SALESREPS\n" +
+                "WHERE ORDER_DATE = HIRE_DATE ;";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"112968", "3978.00", "2007-10-12", "Larry Fitch"});
+
+    }
+
+    @Test
+    public void queryParentChildJoinNotEq_p170() throws SQLException {
+
+        String sqlQuery = "SELECT NAME , QUOTA, CITY, TARGET\n" +
+                "FROM SALESREPS , OFFICES\n" +
+                "WHERE QUOTA > TARGET ";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sue Smith", "350000.00", "Denver", "300000.00"});
+    }
+
+    @Test
+    public void queryParentChildJoinCityFactOverPlan_p171() throws SQLException {
+
+        String sqlQuery = "SELECT CITY , SALES\n" +
+                "FROM OFFICES\n" +
+                "WHERE SALES > TARGET";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"New York", "692637.00"});
+    }
+
+    @Test
+    public void queryParentChildJoinEmplOv350000_p172() throws SQLException {
+
+        String sqlQuery = "SELECT NAME , SALES\n" +
+                "FROM SALESREPS\n" +
+                "WHERE SALES > 350000";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sue Smith", "474050.00"});
+    }
+
+    @Test
+    public void queryParentChildJoinEmplOffices_p172() throws SQLException {
+
+        String sqlQuery = "SELECT NAME , SALESREPS.SALES, CITY\n" +
+                "FROM SALESREPS, OFFICES\n" +
+                "WHERE REP_OFFICE = OFFICE";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sam Clark", "299912.00", "New York"});
+    }
+
+    @Test
+    public void queryParentChildJoinAllCols_p173() throws SQLException {
+
+        String sqlQuery = "SELECT *" +
+                "FROM SALESREPS, OFFICES\n" +
+                "WHERE REP_OFFICE = OFFICE";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"106", "Sam Clark", "52", "11", "VP Sales", "2006-06-14", null, "275000.00", "299912.00", "11", "New York", "Eastern", "106", "575000.00", "692637.00"});
+    }
+
+    @Test
+    public void queryParentChildJoinEmplAllWOffice_p173() throws SQLException {
+
+        String sqlQuery = "SELECT SALESREPS.*, CITY, REGION\n" +
+                "FROM SALESREPS, OFFICES\n" +
+                "WHERE REP_OFFICE = OFFICE";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"106", "Sam Clark", "52", "11", "VP Sales", "2006-06-14", null, "275000.00", "299912.00",  "New York", "Eastern"});
+    }
+
+    @Test
+    public void queryParentChildSelfJoin_p174() throws SQLException {
+
+        String sqlQuery = "SELECT NAME, NAME\n" +
+                "FROM SALESREPS\n" +
+                "WHERE MANAGER = EMPL_NUM";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testNoRows(rs);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"105", "Bill Adams"});
+    }
+
+    @Test
+    public void queryParentChildSelfJoinEmplMgr_p175() throws SQLException {
+
+        String sqlQuery = "SELECT EMPS.NAME, MGRS.NAME\n" +
+                "FROM SALESREPS EMPS, SALESREPS MGRS\n" +
+                "WHERE EMPS.MANAGER = MGRS.EMPL_NUM";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Dan Roberts", "Bob Smith"});
+    }
+
+    @Test
+    public void queryParentChildSelfJoinEmplMgrObeAlias_p175() throws SQLException {
+
+        String sqlQuery = "SELECT SALESREPS.NAME, MGRS.NAME\n" +
+                "FROM SALESREPS, SALESREPS MGRS\n" +
+                "WHERE SALESREPS.MANAGER = MGRS.EMPL_NUM";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Dan Roberts", "Bob Smith"});
+    }
+
+    @Test
+    public void queryParentChildSelfJoinEmplPlanOverMgrPlan_p175() throws SQLException {
+
+        String sqlQuery = "SELECT SALESREPS.NAME, SALESREPS.QUOTA, MGRS.QUOTA\n" +
+                "FROM SALESREPS, SALESREPS MGRS\n" +
+                "WHERE SALESREPS.MANAGER = MGRS.EMPL_NUM\n" +
+                "AND SALESREPS.QUOTA > MGRS.QUOTA";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Dan Roberts", "300000.00", "200000.00"});
+    }
+
+    @Test
+    public void queryParentChildSelfJoinEmplMgrDiffOffices_p176() throws SQLException {
+
+        String sqlQuery = "SELECT EMPS.NAME, EMP_OFFICE.CITY, MGRS.NAME, MGRS_OFFICE.CITY\n" +
+                "FROM SALESREPS EMPS, SALESREPS MGRS, OFFICES EMP_OFFICE, OFFICES MGRS_OFFICE\n" +
+                "WHERE EMPS.REP_OFFICE = EMP_OFFICE.OFFICE\n" +
+                "AND MGRS.REP_OFFICE = MGRS_OFFICE.OFFICE\n" +
+                "AND EMPS.MANAGER = MGRS.EMPL_NUM\n" +
+                "AND EMPS.REP_OFFICE <> MGRS.REP_OFFICE";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Bob Smith", "Chicago", "Sam Clark", "New York"});
+    }
+
+    @Test
+    @Ignore(value = "No birthdays table")
+    public void queryParentChildSelfJoinEmplBD_Aliases_p176() throws SQLException {
+
+        String sqlQuery = "";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{""});
+    }
+
+    @Test
+    public void queryParentChildNamexCity_p179() throws SQLException {
+
+        String sqlQuery = "SELECT NAME, CITY FROM SALESREPS, OFFICES";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Dan Roberts", "New York"});
+    }
+
+    @Test
+    public void queryParentChildNamesWorkingCity_p180() throws SQLException {
+
+        String sqlQuery = "SELECT NAME, CITY FROM SALESREPS, OFFICES WHERE REP_OFFICE = OFFICE";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Sam Clark", "New York"});
+    }
+
+    @Test
+    public void queryParentChildClient2103_p180() throws SQLException {
+
+        String sqlQuery = "SELECT COMPANY , ORDER_NUM, AMOUNT\n" +
+                "FROM CUSTOMERS JOIN ORDERS\n" +
+                "ON CUST_NUM = CUST\n" +
+                "WHERE CUST_NUM = 2103\n" +
+                "ORDER BY ORDER_NUM;";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Acme Mfg.", "112963","3276.00"});
+    }
+    @Test
+    public void queryEmplRepOffice_p182() throws SQLException {
+
+        String sqlQuery = "SELECT NAME, REP_OFFICE FROM SALESREPS";
+
+        ResultSet rs = stmt.executeQuery(sqlQuery);
+        ValidateRowgainstValues.testOnlyOneRow(rs, new String[]{"Dan Roberts", "12"});
+    }
 }
--- home_02/src/test/java/task_02/util/ValidateRowgainstValues.java
+++ home_02/src/test/java/task_02/util/ValidateRowgainstValues.java
@@ -27,9 +27,16 @@ public class ValidateRowgainstValues {
 
         for (int ix = 1; ix<= rsmd.getColumnCount(); ix++) {
             String expectedmsg = String.format("Column %s expected to have value %s, but have %s", rsmd.getColumnName(ix), expectedValues[ix-1], rs.getString(ix));
-            Assert.assertTrue(expectedmsg, expectedValues[ix-1].equals(rs.getString(ix)));
+            if (expectedValues[ix-1] == null) {
+                Assert.assertNull(expectedmsg,rs.getString(ix));
+            } else {
+                Assert.assertTrue(expectedmsg, expectedValues[ix - 1].equals(rs.getString(ix)));
+            }
         }
     }
 
 
+    public static void testNoRows(ResultSet rs) throws SQLException {
+        Assert.assertEquals("No Rows Expected",rs.next(), false);
+    }
 }
